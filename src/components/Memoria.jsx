8aca01c19a9235ed1d04b0326cb1983ab3c97f87
Locoyoccim <charlyhernandez917@gmail.com>
2024-05-01 17:27:05 -0600

    Countri apires RAW Github

--- src/components/Memoria.jsx
+++ src/components/Memoria.jsx
@@ -1,32 +1,27 @@
-import { createContext, useEffect, useState} from "react";
+import { createContext, useEffect, useState } from "react";
 
+export const countryInfo = createContext(null);
 
-export const countryInfo = createContext(null)
+function Memoria({ children }) {
+  const [data, setData] = useState([]);
 
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch("https://raw.githubusercontent.com/Locoyoccim/Countries_JSON/main/public/data.json");
 
-
-function Memoria({children}){
-    const [data, setData] = useState([]);
-
-    useEffect(()=>{
-        const fetchData = async() => {
-            try{ 
-                const response = await fetch('/public/data.json')
-                if(!response.ok){
-                    throw new Error('Error en la carga de datos');
-                }
-                const data = await response.json();
-                setData(data)
-            }catch{
-                console.error('Error', error)
-            }
-        };
-        fetchData();
-    },[])
-    return(
-        <countryInfo.Provider value={data}>
-            {children}
-        </countryInfo.Provider>
-    )
+        if (!response.ok) {
+          throw new Error("Error en la carga de datos");
+        }
+        const data = await response.json();
+        setData(data);
+      } catch {
+        console.error("Error", error);
+      }
+    };
+    fetchData();
+    console.log(data);
+  }, []);
+  return <countryInfo.Provider value={data}>{children}</countryInfo.Provider>;
 }
 export default Memoria;
